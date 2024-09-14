13f2c32b0fb3b893ffb91b4b6db3e88de0b8ac63
abui <adjiem31@gmail.com>
2024-09-14 23:53:02 +0700

    feat(login): add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import './globals.css';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Bytecott",
+  title: 'Bytecott',
   description:
-    "ByteCott helps you make informed choices by tracking global boycott campaigns on products and brands. Join the movement to support ethical consumerism with real-time updates and insights.",
+    'ByteCott helps you make informed choices by tracking global boycott campaigns on products and brands. Join the movement to support ethical consumerism with real-time updates and insights.',
 };
 
 export default function RootLayout({
@@ -26,12 +33,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
