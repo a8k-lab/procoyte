dcf56ea334be7be965071b0c3b65977f46ae834c
abdul <up2dul@gmail.com>
2024-09-15 15:17:55 +0700

    fix: edge runtime pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
-import type React from 'react';
+import type React from "react";
+
+export const runtime = "edge";
 
 const SignInLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
+    <div className="flex items-center justify-center min-h-screen">
       {children}
     </div>
   );
