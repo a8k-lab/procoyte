ded4957e743ac6e5287acb719d4a1389afe223d3
Abdul <malik301002@gmail.com>
2024-09-19 01:59:20 +0700

    style: formatting codebase
    And show header component in main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from 'next';
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { Header } from "@/components/layout/header";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Bytecott',
+  title: "Procoyte | Cek Status Boikot Produk dengan Mudah",
   description:
-    'ByteCott helps you make informed choices by tracking global boycott campaigns on products and brands. Join the movement to support ethical consumerism with real-time updates and insights.',
+    "Cek status boikot produk secara cepat dan mudah. Masukkan tautan atau nama produk untuk melihat apakah masuk daftar boikot terbaru. Dukung kampanye global dan temukan alternatif yang direkomendasikan.",
 };
 
 export default function RootLayout({
@@ -14,16 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <nav className='flex items-center justify-between gap-4 px-4 py-2 sm:px-6 sm:py-4'>
-        Bytecott
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </nav>
-      {children}
+      <Header />
+      <main>{children}</main>
     </>
   );
 }
