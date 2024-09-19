2eb69ec8d6ca84d6ab536afc9b790dd383985497
abdul <up2dul@gmail.com>
2024-09-20 06:16:23 +0700

    chore: extend `xs` screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "480px",
+      },
       fontFamily: {
         poppins: ["var(--font-poppins)"],
         inter: ["var(--font-inter)"],
