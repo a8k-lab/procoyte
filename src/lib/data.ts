864170a98c0665b096a2642c61460ad7e1611fb2
abdul <up2dul@gmail.com>
2024-09-20 22:24:04 +0700

    feat(data): update navLinks href

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
-  { name: "Produk & Brand", href: "/" },
-  { name: "Berita", href: "/" },
-  { name: "FAQs", href: "/" },
-  { name: "Laporkan Produk", href: "/" },
+  { name: "Produk & Brand", href: "/explore" },
+  { name: "Berita", href: "/news" },
+  { name: "FAQs", href: "/faqs" },
+  { name: "Laporkan Produk", href: "/report" },
 ];
