cae7e6f0b6f2aa5158a8c823c1561102bbdde1de
abdul <up2dul@gmail.com>
2024-09-21 11:58:46 +0700

    feat(comps/layout): set footer to be relative

--- src/components/layout/auth-footer.tsx
+++ src/components/layout/auth-footer.tsx
@@ -1,6 +1,6 @@
 export const AuthFooter = () => {
   return (
-    <footer className="py-[25px] w-full text-center text-sm">
+    <footer className="relative py-[25px] w-full text-center text-sm">
       <p>© 2024 Procoyte</p>
     </footer>
   );
