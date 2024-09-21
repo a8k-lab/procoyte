56ba130c088736355831f2772a09f70b77580dc0
abdul <up2dul@gmail.com>
2024-09-21 12:00:11 +0700

    feat(layout): adjust min height to be 100vh-154px

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import { AuthFooter } from "@/components/layout/auth-footer";
 import { AuthHeader } from "@/components/layout/auth-header";
+import { cn } from "@/lib/utils";
 
 export const runtime = "edge";
 
@@ -7,7 +8,13 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <AuthHeader />
-      <main className="relative bg-background flex justify-center sm:bg-transparent sm:my-2.5">
+      <main
+        className={cn(
+          // 154px = AuthHeader height + AuthFooter height (84px + 70px)
+          "min-h-[calc(100vh-154px)] flex items-center justify-center",
+          "relative bg-background sm:bg-transparent sm:py-2.5",
+        )}
+      >
         {children}
       </main>
       <AuthFooter />
