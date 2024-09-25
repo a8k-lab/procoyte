5876231c349782964872bbddc5151cba5ad0c711
abdul <up2dul@gmail.com>
2024-09-26 04:23:57 +0700

    feat(comps/ui): move forwarded className prop to the div parent

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,13 +10,12 @@ export interface InputProps
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, suffixIcon, ...props }, ref) => {
     return (
-      <div className="relative flex items-center gap-1.5">
+      <div className={cn("relative flex items-center gap-1.5", className)}>
         <input
           type={type}
           className={cn(
             "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             suffixIcon && "pr-11",
-            className,
           )}
           ref={ref}
           {...props}
