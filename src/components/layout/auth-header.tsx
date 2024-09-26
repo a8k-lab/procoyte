8de58e9ccf912b3118d035d5feff0d829bfdbc9f
abdul <up2dul@gmail.com>
2024-09-27 06:41:06 +0700

    feat(comps/layout): footer and header bg white on < sm

--- src/components/layout/auth-header.tsx
+++ src/components/layout/auth-header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const AuthHeader = () => {
   return (
-    <header className="flex items-center py-[22px] px-4 sm:justify-center bg-white">
+    <header className="flex items-center bg-white py-[22px] px-4 sm:justify-center sm:bg-transparent">
       <Link href="/" className="flex items-center gap-3 py-2">
         <Image
           src="/images/logo.svg"
