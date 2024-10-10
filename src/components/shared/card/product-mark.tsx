5951879b96b0a064779e6e5cd05415d8a7b9ac3d
abdul <up2dul@gmail.com>
2024-10-11 06:19:47 +0700

    refactor(comps/shared): finishing-up component

--- src/components/shared/card/product-mark.tsx
+++ src/components/shared/card/product-mark.tsx
@@ -1,87 +1,84 @@
 import { Icon } from "@iconify/react";
-import { cva } from "class-variance-authority";
+import sanitizeHtml from "sanitize-html";
 
 import { LinkButton } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-const productMarkCardVariants = cva(
-  "p-6 text-left text-sm text-muted-foreground bg-white border rounded-lg",
-  {
-    variants: {
-      variant: {
-        unmarked: "border-secondary",
-        marked: "border-destructive/50",
-      },
-    },
-    defaultVariants: {
-      variant: "marked",
-    },
-  },
-);
-
 type ProductMarkCardProps =
   | {
-      variant?: "unmarked";
+      isMarked?: false;
     }
   | {
-      variant: "marked";
+      isMarked: true;
+      name: string;
+      owner: string;
+      content: string;
       sources: Array<{
         name: string;
         url: string;
       }>;
+      proofUrl: string;
     };
 
-export const ProductMarkCard = ({ variant }: ProductMarkCardProps) => {
+export const ProductMarkCard = (props: ProductMarkCardProps) => {
   return (
-    <article className={cn(productMarkCardVariants({ variant }))}>
+    <article
+      className={cn(
+        "p-6 text-left text-sm text-muted-foreground bg-white border rounded-lg",
+        props.isMarked ? "border-destructive" : "border-secondary",
+      )}
+    >
       <h1
         className={cn(
           "mb-1.5 font-semibold text-2xl",
-          variant === "unmarked" ? "text-secondary" : "text-destructive",
+          props.isMarked ? "text-destructive" : "text-secondary",
         )}
       >
-        {variant === "unmarked"
-          ? "Produk ini tidak diboikot"
-          : "Produk ini masuk ke daftar boikot!"}
+        {props.isMarked
+          ? "Produk ini masuk ke daftar boikot!"
+          : "Produk ini tidak diboikot"}
       </h1>
 
-      {variant === "unmarked" ? (
-        <p>Produk ini tidak masuk ke daftar boikot.</p>
-      ) : (
+      {props.isMarked ? (
         <div className="flex flex-col items-center gap-6">
           <div>
-            <h2>24s dimiliki oleh LVMH</h2>
+            <h2>
+              {props.name} dimiliki oleh <strong>{props.owner}</strong>.
+            </h2>
 
-            <p className="my-5">
-              Bernard Arnault, the chairman CEO of LMHV Moet Hennesy-Louis
-              Vuitton, and the world’s richest man according to Forbes, has
-              invested in cybersecurity firm, Wiz, the israeli maker of cloud
-              security solutions fir enterprises
-            </p>
+            <p
+              className="my-5"
+              // biome-ignore lint: This is a sanitized string
+              dangerouslySetInnerHTML={{ __html: sanitizeHtml(props.content) }}
+            />
 
             <div className="font-bold">
               <h2>Source:</h2>
-              <ul className="">
-                <li>
-                  1.{" "}
-                  <a
-                    href="https://www.timesofisrael.com/unilever-reports-diving-sales-in-southeast-asia-amid-anti-israel-boycotts/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                    className="underline"
-                  >
-                    Link 1
-                  </a>
-                </li>
+              <ul>
+                {props.sources?.map((source, idx) => (
+                  <li key={source.name}>
+                    {idx + 1}.{" "}
+                    <a
+                      href={source.url}
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      className="underline"
+                    >
+                      {source.name}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
 
-          <LinkButton href="#" variant="destructive">
+          <LinkButton href={props.proofUrl} variant="destructive">
             <Icon icon="majesticons:open-line" className="size-6" />
             Open Proof
           </LinkButton>
         </div>
+      ) : (
+        <p>Produk ini tidak masuk ke daftar boikot.</p>
       )}
     </article>
   );
