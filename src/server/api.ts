881a75ad1720510a721179340b111e269034802a
abdul <up2dul@gmail.com>
2024-10-12 09:17:43 +0700

    feat(api): create brand recommendations fetcher

---
+++ src/server/api.ts
@@ -0,0 +1,28 @@
+import type { BrandsRecord } from "@/xata";
+
+const recommenderBaseUrl = process.env.RECOMMENDER_API_BASE_URL;
+
+export type Recommendation = {
+  target: BrandsRecord;
+  recommendations: BrandsRecord[];
+};
+
+export async function getBrandRecommendations({
+  id,
+  limit = 10,
+}: {
+  id: string;
+  limit?: number;
+}) {
+  if (!recommenderBaseUrl) {
+    throw new Error("RECOMMENDER_API_BASE_URL env is not set");
+  }
+
+  const res = await fetch(`${recommenderBaseUrl}/brands/${id}?limit=${limit}`);
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json() as Promise<Recommendation>;
+}
